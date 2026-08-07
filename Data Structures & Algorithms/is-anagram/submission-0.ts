bd9e06ba7a7cf708f949ac50193a5cd599291b70
mhezzet <40916821+mhezzet@users.noreply.github.com>
2026-08-08 02:20:55 +0300

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.ts
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if(s.length !== t.length) return false
+        
+        let hashS = {}
+        let hashT = {}
+
+        let i = 0
+        while(i < s.length){
+            hashS={...hashS,[s[i]]:hashS[s[i]]? hashS[s[i]]+1:1}
+            hashT={...hashT,[t[i]]:hashT[t[i]]? hashT[t[i]]+1:1}
+            i++
+        }
+
+        for (const [keyS,valueS] of Object.entries(hashS)){
+            if(hashT[keyS] !== valueS){
+                return false
+            }
+        }
+
+        for (const [keyT,valueT] of Object.entries(hashT)){
+            if(hashS[keyT] !== valueT){
+                return false
+            }
+        }
+
+        return true
+
+    }
+}
